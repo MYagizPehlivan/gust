39b9c92fac2dbc087d9be461aee6deeca4b7c327
MYagizPehlivan <m.yagiz.pehlivan@gmail.com>
2023-07-18 21:44:56 +0300

    Changed Position to a tuple struct

--- gust-core/src/world/position.rs
+++ gust-core/src/world/position.rs
@@ -1,16 +1,12 @@
 type Meters = f64;
 
-pub struct Position {
-    /// Latitude in radians.
-    phi: f64,
-
-    /// Longitude in radians.
-    lambda: f64,
-}
+/// Represents a geographical location with
+/// a tuple containing latitude and longitude in radians.
+pub struct Position(f64, f64);
 
 impl std::fmt::Display for Position {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:.3}°, {:.3}°", self.phi.to_degrees(), self.lambda.to_degrees())
+        write!(f, "{:.3}°, {:.3}°", self.0.to_degrees(), self.1.to_degrees())
     }
 }
 
