4cb60fed0eba42596ac2b152eab8bcd0d2a2e201
MYagizPehlivan <m.yagiz.pehlivan@gmail.com>
2023-07-18 20:39:01 +0300

    Changed calculate_skill to an instance method called update_skill

--- gust-core/src/people/skills.rs
+++ gust-core/src/people/skills.rs
@@ -9,18 +9,24 @@ struct Skill {
 
 impl Skill {
     pub fn new(talent: f32, initial_xp: f32) -> Self {
-        Self {
+        debug_assert!(talent > 0.0);
+
+        let mut this = Self {
             talent,
             accumulated_xp: initial_xp,
-            skill: Skill::calculate_skill(talent, initial_xp),
-        }
+            skill: 0.0,
+        };
+
+        this.update_skill();
+
+        this
     }
 
     pub fn gain_xp(&mut self, gained_xp: f32) {
         self.accumulated_xp += gained_xp;
 
         if gained_xp != 0.0 {
-            self.skill = Skill::calculate_skill(self.talent, self.accumulated_xp);
+            self.update_skill();
         }
     }
 
@@ -28,13 +34,13 @@ impl Skill {
         self.skill
     }
 
-    fn calculate_skill(talent: f32, total_xp: f32) -> f32 {
+    fn update_skill(&mut self) {
         // This skill formula draws a curve that features a rapid growth early on,
         // which tapers off as more experience is gained.
         // Skill is bounded between MIN_SKILL and MAX_SKILL.
         // How quickly MAX_SKILL is approached is proportional to talent, which is in the range (0, inf]
         // A talent of 0 restricts skill to 0.
-        MIN_SKILL + (MAX_SKILL - MIN_SKILL) * std::f32::consts::E.powf(-1.0 / (talent * total_xp))
+        self.skill = MIN_SKILL + (MAX_SKILL - MIN_SKILL) * std::f32::consts::E.powf(-1.0 / (self.talent * self.accumulated_xp))
     }
 }
 
