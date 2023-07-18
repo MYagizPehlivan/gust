d730f5b26a09686a8b46ccd864b03a40ce8a5526
MYagizPehlivan <m.yagiz.pehlivan@gmail.com>
2023-07-18 21:49:21 +0300

    Added Position::new

--- gust-core/src/world/position.rs
+++ gust-core/src/world/position.rs
@@ -4,14 +4,20 @@ type Meters = f64;
 /// a tuple containing latitude and longitude in radians.
 pub struct Position(f64, f64);
 
-impl std::fmt::Display for Position {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:.3}°, {:.3}°", self.0.to_degrees(), self.1.to_degrees())
+impl Position {
+    /// Constructs a new position with phi (latitude in radians)
+    /// and lambda (longitude in radians).
+    pub fn new(phi: f64, lambda: f64) -> Self {
+        Self(phi, lambda)
+    }
+
+    pub fn r#move(&mut self, distance: Meters, destination: &Position) {
+        todo!("Implement moving");
     }
 }
 
-impl Position {
-    pub fn r#move(&mut self, distance: Meters, destination: &Position) {
-        todo!("Implement moving");
+impl std::fmt::Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:.3}°, {:.3}°", self.0.to_degrees(), self.1.to_degrees())
     }
 }
